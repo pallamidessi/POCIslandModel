bc80198b41621c9981f8d8fbcb3cd18ca321287d
pallamidessi <joseph.pallamidessi@gmail.com>
2015-10-13 16:54:33 +0200

    Start boost ASIO PoC for GA

---
+++ Makefile
@@ -0,0 +1,21 @@
+CXXFLAGS += -fopenmp -O2 -g -Wall 
+OBJS_SERVER = PoCServer.o
+OBJS_CLIENT = PoCClient.o
+LDFLAGS += -lpthread -fopenmp -lboost_system
+
+TARGET_SERVER = PoCServer
+TARGET_CLIENT = PoCClient
+
+all: $(TARGET_SERVER) $(TARGET_CLIENT)
+
+$(TARGET_SERVER):	$(OBJS_SERVER)
+	$(CXX) -o $(TARGET_SERVER) $(OBJS_SERVER) $(LDFLAGS) -g 
+	
+$(TARGET_CLIENT):	$(OBJS_CLIENT)
+	$(CXX) -o $(TARGET_CLIENT) $(OBJS_SERVER) $(LDFLAGS) -g 
+
+%.o:%.cpp
+	$(CXX) -c $(CXXFLAGS) $^
+
+clean:
+	rm *.o $(TARGET_SERVER) $(TARGET_CLIENT)
---
+++ PoCServer.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstdlib>
+
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/array.hpp>
+
+using boost::asio::ip::udp;
+
+class udp_server {
+  public:
+    udp_server(boost::asio::io_service& io_service)
+        : socket_(io_service, udp::endpoint(udp::v4(), 13)) {
+        start_receive();
+    }
+
+    void start_receive() {
+        socket_.async_receive_from(
+            boost::asio::buffer(recv_buffer_), remote_endpoint_,
+            boost::bind(&udp_server::handle_receive, this,
+                        boost::asio::placeholders::error,
+                        boost::asio::placeholders::bytes_transferred));
+    }
+
+    void handle_receive(const boost::system::error_code& error,
+                        std::size_t /*bytes_transferred*/) {
+        if (!error || error == boost::asio::error::message_size) {
+            std::cout << "Got it" << std::endl;
+            start_receive();
+        }
+    }
+
+    udp::socket socket_;
+    udp::endpoint remote_endpoint_;
+    boost::array<char, 1> recv_buffer_;
+};
+
+int main(int argc, const char *argv[]) {
+
+    try {
+        boost::asio::io_service io_service;
+        udp_server server(io_service);
+        io_service.run();
+    } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
